00cfaff64cfcaa0720edd4a2e395d166970933d6
leesahanders <lisa.anders@rstudio.com>
2022-08-04 16:39:19 -0500

    Updating examples

--- examples/R/resource_owner/app.R
+++ examples/R/resource_owner/app.R
@@ -1,6 +1,8 @@
-# Example using Client Credentials / Client Secret Flow as described in the 'Service Principal' section in https://cran.r-project.org/web/packages/Microsoft365R/vignettes/scripted.html 
+# Example using Resource Owner Flow as described in the 'Service Account' section in https://cran.r-project.org/web/packages/Microsoft365R/vignettes/scripted.html 
 # Authentication and requirements from Azure side (app set up) described in https://cran.r-project.org/web/packages/AzureGraph/vignettes/auth.html 
 # Access to Sharepoint and how to use the various functions described under `Creating a custom app registration` in https://cran.r-project.org/web/packages/Microsoft365R/vignettes/od_sp.html and the API permissions listed under https://github.com/Azure/Microsoft365R/blob/master/inst/app_registration.md
+
+
 
 library(AzureAuth)
 library(AzureGraph)
@@ -11,23 +13,28 @@ library(spsComps)
 
 tenant = Sys.getenv("EXAMPLE_TENANT", "")
 app=Sys.getenv("EXAMPLE_APP", "")
-site_url = "https://rstudioinc.sharepoint.com/sites/integrations-testing"
-drive_name = "Documents" 
+site_url = "https://rstudioinc.sharepoint.com/sites/integrations-testing" # application with user permissions
+drive_name = "Documents"
 file_src = "penguins_raw.csv"
 
 # Add sensitive variables as environmental variables so they aren't exposed
-client_secret <- Sys.getenv("EXAMPLE_SHINY_CLIENT_SECRET", "")
+user <- Sys.getenv("EXAMPLE_MS365R_SERVICE_USER")
+pwd <- Sys.getenv("EXAMPLE_MS365R_SERVICE_PASSWORD")
 
 # Create auth token cache directory, otherwise it will prompt the user on the console for input
 create_AzureR_dir()
 
 foo <- function() {
-  message("Microsoft Graph login and data pull")
+  message("Starting Microsoft Graph login and data pull")
   
-  # Create a Microsoft Graph login
-  gr <- create_graph_login(tenant, app, password=client_secret, auth_type="client_credentials")
+  # create a Microsoft Graph login
+  gr <- create_graph_login(tenant, app, 
+                           username = user, 
+                           password = pwd,
+                           auth_type="resource_owner")
   
-  # Sharepoint site
+  
+  # SharePoint site
   site <- gr$get_sharepoint_site(site_url)
   
   # Note the function get_drive() uses drive_id as a parameter (NOT drive name)
@@ -36,7 +43,7 @@ foo <- function() {
   # Downloading the file:
   drv$download_file(src = file_src, dest = "tmp.csv", overwrite = TRUE)
   
-  # Reading file into memory (it would be a good idea to have handling here for detecting file type)
+  # Reading file into memory
   data <- read.csv(file="tmp.csv", stringsAsFactors = FALSE, check.names=F)
   
   return(data)
@@ -59,6 +66,7 @@ server = function(input,output, session) {
     spsComps::shinyCatch({
       rv$data <- foo()
     },
+
     blocking_level = "error",
     prefix = "My-project" 
     )
